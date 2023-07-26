06c8226de472b88cd13551f44d56e5997b82edf2
karvaroz <vargasriosk@gmail.com>
2023-07-25 22:33:14 -0500

    Villa controller and get function

---
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Pool View"},
+                new Villa { Id = 2, Name = "Beach View"}
+            };
+        }
+    }
+}
