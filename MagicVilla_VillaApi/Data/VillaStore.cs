4c57cd6b2b72f23f96ca33c151240925c1fd2d52
karvaroz <vargasriosk@gmail.com>
2023-07-25 22:59:15 -0500

    first class

--- MagicVilla_VillaApi/Data/VillaStore.cs
+++ MagicVilla_VillaApi/Data/VillaStore.cs
@@ -7,6 +7,7 @@ namespace MagicVilla_VillaApi.Data
         public static List<VillaDTO> villaList = new List<VillaDTO>
         {
             new VillaDTO { Id = 1, Name = "Pool View"},
+
             new VillaDTO { Id = 2, Name = "Beach View"}
         };
     }
